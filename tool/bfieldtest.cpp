90b9ac37a6bbec0bdf0d8d1db6e074879e8c5663
Gero M?ller <Gero M?ller@localhost>
2010-09-09 22:22:35 +0200

    fixed new implementation

--- tool/bfieldtest.cpp
+++ tool/bfieldtest.cpp
@@ -2,18 +2,29 @@
 #include "gadget/Vector3.hpp"
 #include "arguments.hpp"
 
-class TestVisitor: public PagedGrid<Vector3f>::Visitor {
+class TestVisitor1: public PagedGrid<Vector3f>::Visitor {
 public:
 	index3_t center;
 	void visit(PagedGrid<Vector3f> &grid, size_t x, size_t y, size_t z,
 			Vector3f &value) {
-		value.x = (float)center.x - x;
-		value.y = (float)center.y - y;
-		value.z = (float)center.z - z;
+		value.x = (float) center.x - x;
+		value.y = (float) center.y - y;
+		value.z = (float) center.z - z;
 	}
 };
 
-int bfieldtest(Arguments &arguments) {
+class TestVisitor2: public PagedGrid<Vector3f>::Visitor {
+public:
+	index3_t center;
+	void visit(PagedGrid<Vector3f> &grid, size_t x, size_t y, size_t z,
+			Vector3f &value) {
+		value.x -= (float) center.x - x;
+		//value.y -= (float) center.y - y;
+		value.z -= (float) center.z - z;
+	}
+};
+
+void step1(Arguments &arguments) {
 	BinaryPageIO<Vector3f> io;
 	io.setPrefix(arguments.getString("-f", "bfieldtest"));
 	io.setOverwrite(true);
@@ -28,13 +39,41 @@ int bfieldtest(Arguments &arguments) {
 	grid.setPageSize(10);
 	grid.setPageCount(1000);
 
-	TestVisitor v;
-	v.center = index3_t(50);
+	TestVisitor1 v;
+	v.center = index3_t(50, 40, 30);
 	std::cout << "accept..." << std::endl;
 	grid.accept(v, index3_t(0), index3_t(100));
 	std::cout << "flush..." << std::endl;
 	grid.flush();
 	std::cout << "done..." << std::endl;
+}
 
+void step2(Arguments &arguments) {
+	BinaryPageIO<Vector3f> io;
+	io.setPrefix(arguments.getString("-f", "bfieldtest"));
+	//io.setOverwrite(true);
+	io.setElemetsPerFile(50);
+
+	LastAccessPagingStrategy<Vector3f> strategy;
+
+	PagedGrid<Vector3f> grid;
+	grid.setIO(&io);
+	grid.setStrategy(&strategy);
+	grid.setSize(100);
+	grid.setPageSize(10);
+	grid.setPageCount(1000);
+
+	TestVisitor2 v;
+	v.center = index3_t(50, 40, 30);
+	std::cout << "accept..." << std::endl;
+	grid.accept(v, index3_t(0), index3_t(100));
+	std::cout << "flush..." << std::endl;
+	grid.flush();
+	std::cout << "done..." << std::endl;
+}
+
+int bfieldtest(Arguments &arguments) {
+	step1(arguments);
+	step2(arguments);
 	return 0;
 }
