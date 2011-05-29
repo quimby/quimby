5a3581177acdc0b4d7338c35ce0c6b89f17430e1
Gero Müller <geromueller@users.noreply.github.com>
2011-05-29 11:11:21 +0200

    store mass with smoothparticle

--- include/gadget/SmoothParticle.hpp
+++ include/gadget/SmoothParticle.hpp
@@ -9,6 +9,7 @@
 #define SMOOTH_PARTICLE_HPP_
 
 #include <ostream>
+#include <vector>
 #include "gadget/Vector3.hpp"
 
 class SmoothParticle {
@@ -16,6 +17,7 @@ public:
 	Vector3f position;
 	Vector3f bfield;
 	float smoothingLength;
+	float mass;
 
 	static float kernel(float r) {
 		if (r < 0.5) {
@@ -32,16 +34,27 @@ public:
 		return value * kernel(std::fabs((center - position) / hsml));
 	}
 
-	static float kernel(float value, const Vector3f &center, const Vector3f &position, float hsml) {
+	static float kernel(float value, const Vector3f &center,
+			const Vector3f &position, float hsml) {
 		return value * kernel(std::fabs((center - position).length() / hsml));
 	}
 
-	float kernel(const Vector3f &center) {
-		return value * kernel(std::fabs((center - position).length() / smoothingLength));
+	float kernel(const Vector3f &center) const {
+		return kernel(std::fabs((center - position).length() / smoothingLength));
 	}
 
-};
+	float weight() const {
+		8 / (M_PI * smoothingLength * smoothingLength * smoothingLength);
+	}
 
+	float calculateRho(const std::vector<SmoothParticle> &particles) {
+		float rho = 0;
+		for (size_t i = 0; i < particles.size(); i++) {
+			rho += particles[i].mass * particles[i].weight() * particles[i].kernel(position);
+		}
+		return rho;
+	}
+};
 
 inline std::ostream &operator <<(std::ostream &out, const SmoothParticle &v) {
 	out << "(" << v.position.x << "," << v.position.x << "," << v.position.x
