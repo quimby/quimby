be8617e22263aa6e5ce4a1c7d9b6501ba86f61d1
Gero Müller <geromueller@users.noreply.github.com>
2011-10-11 10:35:06 +0200

    cleanup

--- include/gadget/Vector3.hpp
+++ include/gadget/Vector3.hpp
@@ -169,23 +169,6 @@ public:
 			const Vector3<T> &origin = Vector3<T>(0, 0, 0)) const {
 		return (*this -= origin) * f + origin;
 	}
-
-	//
-	//	bool operator <(const Vector3<T> &p) const {
-	//		if (x < p.x)
-	//			return true;
-	//		else if (x > p.x)
-	//			return false;
-	//		if (y < p.y)
-	//			return true;
-	//		else if (y > p.y)
-	//			return false;
-	//		if (z < p.z)
-	//			return true;
-	//		else
-	//			return false;
-	//	}
-
 };
 
 template<>
@@ -214,65 +197,6 @@ inline Vector3<double> &Vector3<double>::operator %=(const double &f) {
 	return *this;
 }
 
-#if 0
-template<typename T>
-Vector3<T> interpolate(const Vector3<T> &a, const Vector3<T> &b) {
-	T la = a.length();
-	T lb = b.length();
-	T d = la + lb;
-	Vector3<T> v = (d - la) * a + (d - lb) * b;
-	v /= d;
-	return v;
-}
-
-template<typename T>
-Vector3<T> interpolate(const std::vector< T > &distances, const vector< Vector3<T> > &values) {
-	if (distances.size() != values.size())
-	throw std::runtime_error("interpolate: no. of positions not equal no. of values!");
-
-	T l = 0;
-	for (size_t i = 0; i < distances.size(); i++)
-	l += distances.size();
-
-	Vector3<T> v(0);
-	for (size_t i = 0; i < distances.size(); i++)
-	v += (l - distances[i]) * values[i];
-
-	v /= l;
-
-	return v;
-}
-
-template<typename T>
-Vector3<T> interpolate(const Vector3<T> &a, const Vector3<T> &b, const Vector3<
-		T> &c, const Vector3<T> &d, const Vector3<T> &e, const Vector3<T> &f,
-		const Vector3<T> &g, const Vector3<T> &h) {
-	T la = a.length();
-	T lb = b.length();
-	T lc = c.length();
-	T ld = d.length();
-	T le = e.length();
-	T lf = f.length();
-	T lg = g.length();
-	T lh = h.length();
-
-	T l = la + lb + lc + ld + le + lf + lg + lh;
-
-	Vector3<T> v(0);
-	v += (l - la) * a;
-	v += (l - lb) * b;
-	v += (l - lc) * c;
-	v += (l - ld) * d;
-	v += (l - le) * e;
-	v += (l - lf) * f;
-	v += (l - lg) * g;
-	v += (l - lh) * h;
-
-	v /= l;
-
-	return v;
-}
-#endif
 template<typename T>
 inline std::ostream &operator <<(std::ostream &out, const Vector3<T> &v) {
 	out << v.x << " " << v.y << " " << v.z;
